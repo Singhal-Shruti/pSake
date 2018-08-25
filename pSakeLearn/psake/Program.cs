50e79d5617c3c755d683c2a6cf656cae99d0a745
shrutisinghal <shrutis@thoughtworks.com>
2018-08-25 15:53:38 +0530

    [INITIAL COMMIT]| Added an empty project

---
+++ pSakeLearn/psake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace psake
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
